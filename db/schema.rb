73f8067d4192f88bf8e7571fba0c44777b183d70
fares meier <fares.meier@hotmail.com>
2024-03-04 15:51:33 +0100

    added omniauth and omniauth-google-oauth2

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_04_143751) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.string "company_link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -26,14 +33,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_04_143751) do
     t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
-
-  create_table "companies", force: :cascade do |t|
-    t.string "name"
-    t.string "company_link"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-
   end
 
 end
