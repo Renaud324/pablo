ef05aed88704315af3f9c83da36dbb0dacea2f9e
fares meier <fares.meier@hotmail.com>
2024-03-04 17:06:44 +0100

    added config for google oauth2 and Gem for .env

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -274,8 +274,10 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   
   config.omniauth :google_oauth2, 
-          Rails.application.credentials.dig(:google_oauth_client_id),
-          Rails.application.credentials.dig(:google_oauth_client_secret)
+                            ENV['GOOGLE_OAUTH_CLIENT_ID'],
+                            ENV['GOOGLE_OAUTH_CLIENT_SECRET'],
+                            {}
+
 
 
   # ==> Warden configuration
