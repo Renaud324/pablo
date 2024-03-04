fd1d2849814cdef869389f629132fbecd3ff8828
fares meier <fares.meier@hotmail.com>
2024-03-04 17:36:07 +0100

    setup callback function in omniauuth callback controllers and setup user creation in User model when an user log in for the first time

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -27,4 +27,23 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # def after_omniauth_failure_path_for(scope)
   #   super(scope)
   # end
+  def google_oauth2
+    user = User.from_omniauth(auth)
+    if user.present?
+      sign_out_all_scopes
+      flash[:success] = t 'devise_omniauth_callbacks.success', kind: 'Google'
+      sign_in_and_redirect user, event: :authentication
+    else
+      flash[:alert] = 
+        t 'devise_omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized"
+      redirect_to_new_user_session_path
+    end
+  end
+  
+  private
+  
+  def auth
+    auth ||= request.env['omniauth.auth']
+  end
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [:google_oauth2]
+    devise :database_authenticatable, :registerable,
+           :recoverable, :rememberable, :validatable,
+           :omniauthable, omniauth_providers: [:google_oauth2]
+
+    def self.from_omniauth(auth)
+     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+       user.email = auth.info.email
+       user.password = Devise.friendly_token[0,20]
+       user.fullname = auth.info.name
+       user.avatar_url = auth.info.image
+    end
+  end
 end
