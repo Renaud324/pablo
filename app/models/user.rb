9a71d668815fde489706c7feab057f16fd924ad3
fares meier <fares.meier@hotmail.com>
2024-03-06 17:27:32 +0100

    has many interactions added to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
     has_many :applications
+    has_many :interactions
 
     devise :database_authenticatable, :registerable,
            :recoverable, :rememberable, :validatable,
