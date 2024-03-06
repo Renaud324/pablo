97582988ee25a1dab6d29d4fafcdd2330a9250ca
Clemence Luc <clemence.luc.23@gmail.com>
2024-03-06 15:33:25 +0100

    added pipeline in home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,5 +6,8 @@ class PagesController < ApplicationController
     @job_applications = JobApplication.where(user_id: current_user.id)
     @tasks = Task.where(job_application_id: @job_applications.ids)
     @task = Task.new
+    @just_applied_applications = JobApplication.where(status: 'Just Applied')
+    @first_interview_applications = JobApplication.where(status: 'First Interview')
+    @advanced_process_applications = JobApplication.where(status: 'Advanced Process')
   end
 end
