8688fa1278f2383817115e1d4bfba95c172fb9c6
Clemence Luc <clemence.luc.23@gmail.com>
2024-03-06 11:07:07 +0100

    to do for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,6 +3,13 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  # create
+  # 1 recuepre job application de cette task -> @jobapplication where current user id = gna
+  # 2 instancie nouvelle task job application id = celle du dessus (selectionne job application dans formulaire)
+
+
+
+
   private
 
   def task_params
