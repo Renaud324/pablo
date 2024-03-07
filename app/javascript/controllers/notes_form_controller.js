f97f2c1a06d96e5bc8647b62fafdedf2435117fc
Clemence Luc <clemence.luc.23@gmail.com>
2024-03-07 13:06:32 +0100

    add notes

---
+++ app/javascript/controllers/notes_form_controller.js
@@ -0,0 +1,39 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="notes-form"
+export default class extends Controller {
+  connect() {
+    console.log("suis dans notes JS controller");
+  }
+
+  submit(event) {
+    event.preventDefault(); // Prevent default form submission
+    const form = event.target;
+
+    fetch(form.action, {
+      method: form.method,
+      body: new FormData(form),
+      headers: {
+        'Accept': 'application/json'
+      }
+    })
+    .then(response => {
+      if (response.ok) {
+        // Handle successful response
+        return response.json(); // Parse JSON response
+      } else {
+        // Handle error response
+        console.error('Failed to save notes');
+        throw new Error('Failed to save notes');
+      }
+    })
+    .then(data => {
+      // Handle JSON data
+      console.log('Notes saved successfully:', data);
+      this.element.outerHTML = data.notes
+    })
+    .catch(error => {
+      console.error('An error occurred:', error);
+    });
+  }
+}
