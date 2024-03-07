18382455f5592eafe8cfd2a95d9de13b194a170b
Ophelie Bazard <ophelie.bazard@outlook.fr>
2024-03-07 12:07:01 +0100

    sidebar ok

--- app/javascript/controllers/sidebar.js
+++
@@ -1,9 +0,0 @@
-const sidebar = body.querySelector(".sidebar"),
-  toggle = body.querySelector(".toggle"),
-  searchBtn = body.querySelector(".search-box"),
-  modeSwitch = body.querySelector(".mode"),
-  modeText = body.querySelector(".mode-text");
-
-toggle.addEventListener("click", () => {
-  sidebar.classList.toggle("close");
-});
--- app/javascript/controllers/sidebar_controller.js
+++ app/javascript/controllers/sidebar_controller.js
@@ -2,13 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="sidebar-deploy"
 export default class extends Controller {
+
   connect() {
     console.log("Hello from the other siiiiide");
   }
-}
-//   static targets = ["menu"];
 
-//   toggleMenu() {
-//     this.menuTarget.classList.toggle("close");
-//   }
-// }
+  toggleMenu() {
+    this.element.classList.toggle("close");
+  }
+}
