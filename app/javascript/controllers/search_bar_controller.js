8b1b8eeb694ff1c56a5eeec9938166352c9f7aab
ahmed boussaada <ahmedboussaada@hotmail.fr>
2024-03-07 17:59:00 +0100

    trying to upload the AJAX search

--- app/javascript/controllers/search_bar_controller.js
+++ app/javascript/controllers/search_bar_controller.js
@@ -2,19 +2,27 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="search-bar"
 export default class extends Controller {
-
-  static targets = ["query"]
+  static targets = ["form", "input", "list"]
 
   connect() {
-    console.log ("i am connected to the search controller")
+    console.log(
+      "connected to the search-bar controller"
+    );
   }
 
-  clearQuery(event) {
-    if (this.queryTarget.value.trim() === "") {
-      event.preventDefault();
-      this.element.action = "/";
-      this.element.submit();
+  search(event) {
+    // Prevent fetching when the input is empty
+    if (this.inputTarget.value.trim() === "") {
+      this.listTarget.innerHTML = ""; // Clear previous results or set to default state
+      return;
     }
+
+    const url = `${this.formTarget.action}?query=${encodeURIComponent(this.inputTarget.value)}`;
+    fetch(url, {headers: {"Accept": "text/plain"}})
+      .then(response => response.text())
+      .then((data) => {
+        this.listTarget.outerHTML = data;
+      });
   }
 
 }
