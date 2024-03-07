c81123ac22de31335d200bc1b62ac84fbe15bb4a
Renaud de Peretti <renaud.deperetti@hotmail.fr>
2024-03-07 12:55:02 +0100

    Add create interaction button

--- app/controllers/interactions_controller.rb
+++ app/controllers/interactions_controller.rb
@@ -1,5 +1,28 @@
 class InteractionsController < ApplicationController
   def index
-    @interactions = Interaction.all
+    @interactions = current_user.interactions
+    @interaction = Interaction.new
+
   end
+
+  def create
+    temp_params=interaction_params
+    temp_params[:interaction_type] = temp_params[:interaction_type].to_i
+    @interaction = Interaction.new(temp_params)
+    @interaction.user = current_user
+
+    if @interaction.save
+      # redirect_to @interaction, notice: 'Interaction was successfully created.'
+      # redirect_to root_path
+      redirect_to interactions_path
+    end
+  end
+
+
+  private
+
+  def interaction_params
+    params.require(:interaction).permit(:headline, :event_date, :event_time, :location, :interaction_type, :job_application_id)
+  end
+
 end
