c926c35c7d077b5729d6cb24f3246a2075ad9c85
fares meier <fares.meier@hotmail.com>
2024-03-08 14:53:59 +0100

    changed sidekiq.rb to setup redis and sidekiq for production

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = { url: ENV['REDISCLOUD_URL'] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = { url: ENV['REDISCLOUD_URL'] }
 end
