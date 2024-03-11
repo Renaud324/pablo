761cb162dbe516a614688525763f15aef287edf1
Clemence Luc <clemence.luc.23@gmail.com>
2024-03-11 12:25:37 +0100

    close modal fixed

--- app/javascript/controllers/interaction_controller.js
+++ app/javascript/controllers/interaction_controller.js
@@ -3,30 +3,44 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="interaction"
 export default class extends Controller {
   static targets = ['form']
-  static values = { applicationId: Number }
+  static values = { id: Number }
 
   connect() {
     console.log('hello')
+    console.log(this.idValue)
   }
 
-  async create(evt) {
+  create(evt) {
     evt.preventDefault();
+    console.log(document.getElementById('modal').dataset.interactionIdValue)
 
-    const options = {
+    fetch(`/job_applications/${document.getElementById('modal').dataset.interactionIdValue}/interactions`, {
       method: "POST", // Could be dynamic with Stimulus values
       headers: { "Accept": "application/json" },
       body: new FormData(this.formTarget)
-    }
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.status == 'ok') {
+        // close modal behavior
+        this.closeModal()
+      } else {
+        this.element.innerHTML = data.html
+      }
+    })
+  }
 
-    const response = await fetch(`/job_applications/${this.applicationIdValue}/interactions`, options)
-    const data = await response.json();
-
-    if (data.status == 'ok') {
-      // close modal behavior
-      this.formTarget.classList.remove("show");
-        this.formTarget.style.display = "none";
-    } else {
-      this.element.innerHTML = data.html
+  closeModal() {
+    if (this.element) {
+        document.querySelector('body').classList.remove('modal-open');
+        document.querySelector('body').style.overflow = "auto";
+        this.element.classList.remove('show');
+        this.element.setAttribute('aria-hidden', 'true');
+        this.element.style.display = 'none';
+        var modalBackdrop = document.getElementsByClassName('modal-backdrop')[0];
+        if (modalBackdrop) {
+            modalBackdrop.remove();
+        }
     }
   }
 }
