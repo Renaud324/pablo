d2d9abfbc79d2c6ca5e724a5bee981cb038171c0
fares meier <fares.meier@hotmail.com>
2024-03-13 10:29:19 +0100

    Modified seeds, added more job_application at status 0, seed is more now coherent

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,131 +9,203 @@ Contact.destroy_all
 Company.destroy_all
 
 # ########################-USER-###########################
-puts "seeding the user"
-user1 = User.where(email: "aloha.alice.pablo@gmail.com").first
+puts "Finding the user..."
+  user1 = User.where(email: "aloha.alice.pablo@gmail.com").first
 
-company1 = Company.create!(
-  name: 'Monday.com',
-  company_link: 'https://www.monday.com'
-)
 
 # ########################-COMPANIES-###########################
 puts "seeding the companies"
 
-company2 = Company.create(
-  name: 'Tech Innovations',
-  company_link: 'https://techinnovations.com'
+  company1 = Company.create!(
+    name: 'Monday.com',
+    company_link: 'https://www.monday.com'
   )
 
-company3 = Company.create(
-  name: 'Google',
-  company_link: 'https://google.com'
+
+  company2 = Company.create(
+    name: 'Tech Innovations',
+    company_link: 'https://techinnovations.com'
+  )
+
+  company3 = Company.create(
+    name: 'Google',
+    company_link: 'https://google.com'
   )
 
 # ########################-CONTACTS-###########################
-puts "seeding the contacts"
+  puts "seeding the contacts"
 
-contact1 = Contact.create(
-  email: 'contact1@company1.com',
-  phone_number: '123-456-7890',
-  company: company2
+  contact1 = Contact.create(
+    email: 'adrien-hr@monday.com',
+    phone_number: '123-456-7890',
+    job_title: 'Human Resources Manager',
+    company: company1
   )
 
-contact2 = Contact.create(
-  email: 'contact2@company2.com',
-  phone_number: '098-765-4321',
-  company: company3)
+  contact2 = Contact.create(
+    email: 'bob@techinnovations.com',
+    phone_number: '123-456-7890',
+    job_title: 'Human Resources Manager',
+    company: company2
+   )
+
+  contact3 = Contact.create(
+    email: 'recrutement@google.com',
+    phone_number: '098-765-4321',
+    job_title: 'Human Resources Manager',
+    company: company3
+  )
+
+  contact4 = Contact.create(
+    email: 'ahmedboussaada1@gmail.com',
+    phone_number: '098-765-4321',
+    job_title: 'Human Resources Manager',
+    company: company3
+  )
 
 # ########################-APPLICATIONS-###########################
 puts "seeding the applications"
 
-job_application1 = JobApplication.create!(
-  job_title: 'Software Engineer',
-  offer_link: 'https://www.companya.com/job/123',
-  status: 1,
-  job_location: 'New York',
-  notes: 'Application sent',
-  job_description: 'Description of the job...',
-  salary: 80000,
-  application_source: 'LinkedIn',
-  user: user1,
-  company: company1
+  job_application1 = JobApplication.create!(
+    job_title: 'Next.js Back-end Developer',
+    offer_link: 'https://fr.indeed.com/jobs?q=Next.js&l=Paris+%2875%29&start=10&vjk=6ace7e6212caca95',
+    status: 0,
+    job_location: 'New York',
+    notes: 'Application sent',
+    job_description: 'Develop and maintain web applications using Next.js. Next.js and React.js experience required. Supabase experience required, Docker and Kubernetes experience a plus. AWS experience a plus.',
+    salary: 80000,
+    application_source: 'Indeed',
+    user: user1,
+    company: company1
 )
 
 
-job_application2 = JobApplication.create!(
-  application_start_date: Date.today,
-  job_title: 'Software Engineer',
-  offer_link: 'https://jobs.example.com/se',
-  status: 0,
-  job_location: 'Remote',
-  notes: 'First round interview completed.',
-  job_description: 'Develop and maintain web applications.',
-  salary: 85000.0,
-  application_source: 'LinkedIn',
-  user: user1,
-  company: company2
+  job_application2 = JobApplication.create!(
+    application_start_date: Date.today,
+    job_title: 'Software Engineer',
+    offer_link: 'https://fr.indeed.com/jobs?q=Software+engineer&l=Paris+%2875%29&vjk=17e01ca92f35af51&advn=4200815118720440',
+    status: 0,
+    job_location: 'Remote',
+    notes: 'First round interview completed.',
+    job_description: 'Develop and maintain web applications.',
+    salary: 85000.0,
+    application_source: 'Indeed',
+    user: user1,
+    company: company2
   )
 
-job_application3 = JobApplication.create!(
-  application_start_date: Date.today - 5,
-  job_title: 'Product Manager',
-  offer_link: 'https://jobs.example.com/pm',
-  status: 2,
-  job_location: 'New York, NY',
-  notes: 'Awaiting response.',
-  job_description: 'Lead product development teams.',
-  salary: 95000.0,
-  application_source: 'Indeed',
-  user: user1,
-  company: company3
-)
+  job_application3 = JobApplication.create!(
+    application_start_date: Date.today - 5,
+    job_title: 'PHP Back-end Developer',
+    offer_link: 'https://www.welcometothejungle.com/fr/companies/sii/jobs/developpeur-php-back-end-f-h_sophia-antipolis?q=6788722412bf473880c78f3673414aaa&o=cb5d3fe9-7fc4-44e0-b297-d0dfb4e8f15b',
+    status: 0,
+    job_location: 'New York, NY',
+    notes: 'Awaiting response.',
+    job_description: 'Develop and maintain web applications using PHP. Laravel experience required. Docker and Kubernetes experience a plus. AWS experience a plus.',
+    salary: 85000,
+    application_source: 'Welcome to the jungle',
+    user: user1,
+    company: company3
+  )
 
-job_application4 = JobApplication.create!(
-  application_start_date: Date.today - 5,
-  job_title: 'Product Owner',
-  offer_link: 'https://jobs.example.com/pm',
-  status: 0,
-  job_location: 'Paris',
-  notes: 'Awaiting response.',
-  job_description: 'Google software engineers develop the next-generation technologies that change how billions of users connect, explore, and interact with information and one another. Our products need to handle information at massive scale, and extend well beyond web search. We are looking for engineers who bring fresh ideas from all areas, including information retrieval, distributed computing, large-scale system design, networking and data storage, security, artificial intelligence, natural language processing, UI design and mobile; the list goes on and is growing every day. As a software engineer, you will work on a specific project critical to Google’s needs with opportunities to switch teams and projects as you and our fast-paced business grow and evolve. We need our engineers to be versatile, display leadership qualities and be enthusiastic to take on new problems across the full-stack as we continue to push technology forward.
+  job_application4 = JobApplication.create!(
+    application_start_date: Date.today - 5,
+    job_title: 'Node.js Back-end Developer',
+    offer_link: 'https://www.welcometothejungle.com/fr/companies/agorastore-1/jobs/developpeur-senior-backend-node-js-h-f_montreuil?q=afde50d21e921b4f9ac1738c08a46a44&o=fdd49676-d091-4505-8e10-0690b57d61c3',
+    status: 0,
+    job_location: 'New York, NY',
+    notes: 'Awaiting response.',
+    job_description: 'Develop and maintain web applications using Node.js. Nest.js experience required. Docker and Kubernetes experience a plus. AWS experience a plus.',
+    salary: 75000,
+    application_source: 'Welcome to the jungle',
+    user: user1,
+    company: company3
+  )
 
-  With your technical expertise you will manage project priorities, deadlines, and deliverables. You will design, develop, test, deploy, maintain, and enhance software solutions.
+  job_application5 = JobApplication.create!(
+    application_start_date: Date.today - 5,
+    job_title: 'Rust Developer',
+    offer_link: 'https://www.welcometothejungle.com/fr/companies/stormshield/jobs/developpeur-rust-h-f_lille?q=1bc281bcb2dba4dc5024fcd369db9317&o=50e77273-9b76-4de5-b208-6b47e2a0705e',
+    status: 0,
+    job_location: 'New York, NY',
+    notes: 'Awaiting response.',
+    job_description: 'Develop and maintain web applications using Rust. Actix experience required. Docker and Kubernetes experience a plus. AWS experience a plus.',
+    salary: 140000,
+    application_source: 'Welcome to the jungle',
+    user: user1,
+    company: company3
+  )
 
-  Google Cloud accelerates organizations ability to digitally transform their business with the best infrastructure, platform, industry solutions and expertise. We deliver enterprise-grade solutions that leverage Google’s cutting-edge technology – all on the cleanest cloud in the industry. Customers in more than 200 countries and territories turn to Google Cloud as their trusted partner to enable growth and solve their most critical business problems.',
-  salary: 100000,
-  application_source: 'Indeed',
-  user: user1,
-  company: company3
-)
 
-job_application5 = JobApplication.create!(
-  application_start_date: Date.today - 5,
-  job_title: 'Junior Dev',
-  offer_link: 'https://jobs.example.com/pm',
-  status: 3,
-  job_location: 'Madrid',
-  notes: 'Awaiting response.',
-  job_description: 'Lead product development teams.',
-  salary: 30000,
-  application_source: 'Indeed',
-  user: user1,
-  company: company2
-)
+  job_application6 = JobApplication.create!(
+    application_start_date: Date.today - 5,
+    job_title: 'Product Manager',
+    offer_link: 'https://www.welcometothejungle.com/fr/companies/hubcycle/jobs/product-manager_avignon?q=7060af97573be2628b70b6c750bf44e8&o=587d862f-ddb2-45fb-8d2e-623a11f6908d',
+    status: 1,
+    job_location: 'New York, NY',
+    notes: 'Awaiting response.',
+    job_description: 'Lead product development teams.',
+    salary: 95000.0,
+    application_source: 'Welcome to the jungle',
+    user: user1,
+    company: company3
+  )
 
-job_application6 = JobApplication.create!(
-  application_start_date: Date.today - 5,
-  job_title: 'Fullstack Dev',
-  offer_link: 'https://jobs.example.com/pm',
-  status: 3,
-  job_location: 'London',
-  notes: 'Awaiting response.',
-  job_description: 'Lead product development teams.',
-  salary: 60000,
-  application_source: 'Linkedin',
-  user: user1,
-  company: company1
-)
+  job_application7 = JobApplication.create!(
+    application_start_date: Date.today - 5,
+    job_title: 'Product Owner',
+    offer_link: 'https://www.welcometothejungle.com/fr/companies/hellowork/jobs/product-owner-h-f_rennes_HELLO_wJGx9yD?q=5f0a3390a9a7146222e6257994ed60e9&o=f4593631-e9b1-4985-aa66-af9256a32125',
+    status: 1,
+    job_location: 'Paris',
+    notes: 'Awaiting response.',
+    job_description: 'Coordinate and lead product development teams to align with business goals and user needs. Define product vision, roadmap, and prioritize features. Conduct market research and gather feedback to drive product innovation and enhancements. Collaborate with stakeholders including engineering, design, and marketing to ensure successful product delivery. Facilitate communication and foster a collaborative environment within the team. Continuously monitor and analyze product performance metrics to iterate and improve product strategy.',
+    salary: 100000,
+    application_source: 'Welcome to the jungle',
+    user: user1,
+    company: company3
+  )
+
+  job_application8 = JobApplication.create!(
+    application_start_date: Date.today - 5,
+    job_title: 'Junior React.js Developer',
+    offer_link: 'https://fr.indeed.com/jobs?q=React.js+junior&l=Paris+%2875%29&vjk=2b146900fbea55be',
+    status: 1,
+    job_location: 'Madrid',
+    notes: 'Awaiting response.',
+    job_description: 'Develop user interfaces using React.js. Collaborate with the development team to design and implement front-end features. Write clean, maintainable, and efficient code. Troubleshoot and debug issues to ensure smooth functionality. Stay updated on emerging technologies and best practices in web development.',
+    salary: 30000,
+    application_source: 'Indeed',
+    user: user1,
+    company: company2
+  )
+
+  job_application9 = JobApplication.create!(
+    application_start_date: Date.today - 5,
+    job_title: 'Fullstack Developper (React.js/Spring Boot)',
+    offer_link: 'https://fr.indeed.com/jobs?q=react+spring+boots&l=Paris+%2875%29&vjk=45349f66f517445f&advn=4190676325902957',
+    status: 2,
+    job_location: 'Paris, France',
+    notes: 'Awaiting response.',
+    job_description: "Develop and maintain web applications using React.js and Spring Boot. Collaborate with cross-functional teams to define, design, and ship new features. Ensure the performance, quality, and responsiveness of applications. Identify and correct bottlenecks and fix bugs. Help maintain code quality, organization, and automatization.",
+    salary: 70000,
+    application_source: 'Indeed',
+    user: user1,
+    company: company1
+  )
+
+  job_application10 = JobApplication.create!(
+    application_start_date: Date.today - 5,
+    job_title: 'Blockchain Developper',
+    offer_link: 'https://getapplicantai.com/synthr-ou/senior-smart-contract-developer/99?ref=web3.career#apply',
+    status: 2,
+    job_location: 'Remote',
+    notes: 'Awaiting response.',
+    job_description: 'Develop and maintain web applications using blockchain technology. Collaborate with cross-functional teams to define, design, and ship new features. Ensure the performance, quality, and responsiveness of applications. Identify and correct bottlenecks and fix bugs. Help maintain code quality, organization, and automatization.',
+    salary: 200000,
+    application_source: 'Web3 Career',
+    user: user1,
+    company: company1
+  )
 
 # ########################-TASKS-###########################
 puts "seeding the tasks"
@@ -142,8 +214,8 @@ task1 = Task.create!(
   name: 'Update Resume',
   description: 'Revise and update my resume to better match the job requirements.',
   deadline_date: Date.today + 7,
-  status: :pending, # using the enum
-  completion_date: nil,
+  status: :pending,
+  completion_date: nil, 
   job_application: job_application1
 )
 
