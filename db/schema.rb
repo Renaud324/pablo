22a6930fc1e666e7a040176b3a243bc6adc8b482
fares meier <fares.meier@hotmail.com>
2024-03-13 11:35:34 +0100

    did migration to change status column in job_application table from string to integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_13_092227) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_13_100456) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -97,7 +97,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_13_092227) do
     t.datetime "application_start_date"
     t.string "job_title"
     t.string "offer_link"
-    t.string "status"
+    t.integer "status"
     t.string "job_location"
     t.text "notes"
     t.text "job_description"
