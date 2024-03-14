4a30317297ffadf3992c630be7237761d4f417c4
fares meier <fares.meier@hotmail.com>
2024-03-14 10:18:05 +0100

    initialize dragndrop controller, selected column to implement dragndrop feature

--- app/javascript/controllers/dragndrop_controller.js
+++ app/javascript/controllers/dragndrop_controller.js
@@ -1,29 +1,17 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
+import Sortable from "sortablejs";
 
 export default class extends Controller {
-  static targets = ["card"];
+
+  static targets = ["column1", "column2", "column3"];
 
   connect() {
-    this.cardTargets.forEach(card => {
-      card.addEventListener("dragstart", this.dragStart.bind(this));
-      card.addEventListener("dragover", this.dragOver.bind(this));
-      card.addEventListener("drop", this.drop.bind(this));
-    });
+    console.log("Drag and drop controller connected");
+    
   }
 
-  dragStart(event) {
-    event.dataTransfer.setData("text/plain", event.target.dataset.index);
-  }
-
-  dragOver(event) {
+  click(event){
     event.preventDefault();
-  }
-
-  drop(event) {
-    event.preventDefault();
-    const index = event.dataTransfer.getData("text/plain");
-    const card = document.querySelector(`[data-index='${index}']`);
-    const container = event.target.closest(".container");
-    container.insertBefore(card, event.target);
+    console.log("clicked column" + event.target.id);
   }
 }
