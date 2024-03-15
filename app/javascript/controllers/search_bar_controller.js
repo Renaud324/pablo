a569f09e8fac8b7c942c964ceb87b6017592e832
fares meier <fares.meier@hotmail.com>
2024-03-15 09:57:25 +0100

    disabled searchbar controller stimulus to resolved bug in production

--- app/javascript/controllers/search_bar_controller.js
+++ app/javascript/controllers/search_bar_controller.js
@@ -1,21 +1,22 @@
-import { Controller } from "@hotwired/stimulus"
-
-export default class extends Controller {
-  static targets = ["form", "input", "list", "tasks"]
-
-  connect() {
-  }
-
-  search(event) {
-
-    const url = `${this.formTarget.action}?query=${encodeURIComponent(this.inputTarget.value)}`;
-    fetch(url, { headers: { "Accept": "application/json" } })
-      .then(response => response.json())
-      .then((data) => {
-        console.log(data.list_html)
-        this.tasksTarget.outerHTML = data.tasks_html;
-        this.listTarget.outerHTML = data.list_html;
-      });
-  }
-}
+// import { Controller } from "@hotwired/stimulus"
+// 
+// export default class extends Controller {
+//   static targets = ["form", "input", "list", "tasks"]
+// 
+//   connect() {
+//   }
+// 
+//   search(event) {
+// 
+//     const url = `${this.formTarget.action}?query=${encodeURIComponent(this.inputTarget.value)}`;
+//     fetch(url, { headers: { "Accept": "application/json" } })
+//       .then(response => response.json())
+//       .then((data) => {
+//         console.log(data.list_html)
+//         console.log("salut");
+//         this.tasksTarget.outerHTML = data.tasks_html;
+//         this.listTarget.outerHTML = data.list_html;
+//       });
+//   }
+// }
 
